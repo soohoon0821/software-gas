e02835a198749a4c5bd70837da350e9312ef4e46
soohoon0821 <152082740+soohoon0821@users.noreply.github.com>
2023-11-27 15:01:00 +0900

    software gas

---
+++ Assets/gas/Switch.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Switch : MonoBehaviour
+{
+    public Image On;
+    public Image Off;
+   
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ON()
+    {
+        
+        Off.gameObject.SetActive(true);
+        On.gameObject.SetActive(false);
+    }
+    public void OFF()
+    {
+        
+        On.gameObject.SetActive(true);
+        Off.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/gas/timeset.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timeset : MonoBehaviour
+{
+    public Text ScriptTxt;
+
+    int gold = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ScriptTxt.text = "0";
+    }
+
+    // Update is called once per frame
+    public void numplus()
+    {
+        gold += 1;
+        ScriptTxt.text = gold.ToString();
+    }
+    public void nummin()
+    {
+
+        gold -= 1;
+        if (gold < 0)
+            gold = 0;
+        ScriptTxt.text = gold.ToString();
+    }
+}
